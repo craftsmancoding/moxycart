b8d9c975ffaafdc026dfd611a30f64b01d687e83
dan.edan <INTEL@INTEL-PC.(none)>
2013-11-18 05:16:58 +0800

    Update getDatafeed snippet and foxycartdatafeed.class

--- core/components/moxycart/elements/snippets/getDataFeed.snippet.php
+++ core/components/moxycart/elements/snippets/getDataFeed.snippet.php
@@ -14,7 +14,33 @@
 */
 
 $core_path = $modx->getOption('moxycart.core_path', null, MODX_CORE_PATH);
-$class_path = $core_path . 'components/moxycart/model/moxycart/foxycartdatafeed.class.php';
-require_once($class_path);
-$fc_datafeed = new FC_Datafeed($modx);
-return $fc_datafeed->get_datafeed();
+
+require_once($core_path . 'components/moxycart/model/moxycart/foxycartdatafeed.class.php');
+require_once($core_path . 'components/moxycart/model/moxycart/rc4crypt.class.php');
+$rc4crypt = new rc4crypt();
+$fc_datafeed = new FC_Datafeed($rc4crypt);
+
+$error_msgs = array();
+
+$api_key = $modx->getOption('moxycart.api_key'); // your foxy cart datafeed key
+if(empty($api_key)) {
+	$err_msg = 'Foxycart API Key is empty on your System Setting';
+	$error_msgs[] = $err_msg;
+	$modx->log($level,$err_msg,$this->log_target,__CLASS__.'::'.__FUNCTION__,__FILE__,__LINE__);
+}
+
+// You can change the following data if you want to customize what data gets written.
+if($data = $modx->getOption('FoxyData', $_POST)) {
+	$xml = $fc_datafeed->decrypt($data,$api_key);
+
+	// processed the xml
+	$fc_datafeed->parseXML($xml);
+
+	return 'foxy';
+
+} else {
+	$err_msg = 'Failed to Proccessed Transaction Data Feed.Please check Error Logs on Foxycart Admin';
+	$error_msgs[] = $err_msg;
+	$modx->log($level,$err_msg,$this->log_target,__CLASS__.'::'.__FUNCTION__,__FILE__,__LINE__);
+	return 'error';
+}
--- core/components/moxycart/model/moxycart/foxycartdatafeed.class.php
+++ core/components/moxycart/model/moxycart/foxycartdatafeed.class.php
@@ -12,65 +12,15 @@
 * and processed the data without going through foxycart Ecommerce Process
 * --------------------------------------------------
 **/
+
 class FC_Datafeed {
 
-	/** @var $modx modX */
-    public $modx;
+	public $rc4crypt;
 
-    public $lifetime;
-    public $cache_opts;
-
-    public $error_msgs = array();
-
-    private $core_path;
-    private $log_target;
-
-
-    public function __construct(&$modx) 
-    {
-        $this->modx =& $modx;       
-		$this->core_path = $this->modx->getOption('moxycart.core_path', null, MODX_CORE_PATH);
-		$cache_dir = 'moxycart_datafeed';
-		$this->lifetime = 0;
-		$this->cache_opts = array(xPDO::OPT_CACHE_KEY => $cache_dir);
-		$this->log_target = array(
-		    'target'=>'FILE',
-		    'options' => array(
-		        'filename'=>'foxycart.log'
-		    )
-		); 
-		$this->__required_class();
-    }
-
-    /**
-	* Private function __required_class
-	* This will include rc4crypt, SimpleXMLElement adn DomDocument Class
-	* Set error message on Log file
-    **/
-    private function __required_class() 
-    {
-    	$required_classes = array('rc4crypt','SimpleXMLElement','DomDocument');
-    	$rc4crypt_path = $this->core_path . 'components/moxycart/model/moxycart/rc4crypt.class.php';
-		require_once($rc4crypt_path);
-
-    	foreach($required_classes as $c) {
-    		if( !class_exists($c)) {
-    		  $this->__log_errors('Missing Required '. $c .' Class');
-			}
-    	}
-    }
-
-    /**
-	* Private function __required_class
-	* This will include rc4crypt, SimpleXMLElement adn DomDocument Class
-	* Set error message on Log file
-    **/
-    private function __log_errors($err_msg,$level=1) 
-    {
-		$this->error_msgs[] = $err_msg;
-		$this->modx->log($level,$err_msg,$this->log_target,__CLASS__.'::'.__FUNCTION__,__FILE__,__LINE__);
-    }
-
+	public function __construct(rc4crypt $rc4crypt) 
+	{
+		$this->rc4crypt = $rc4crypt;
+	}
     /**
 	* decrypt function
 	* Decrypt data using rc4crypt class
@@ -78,40 +28,12 @@ class FC_Datafeed {
 	* @return xml $xml
     **/
     public function decrypt($data,$api_key) {
-    	$rc4crypt = new rc4crypt();
     	$FoxyData_encrypted = urldecode($data);
-		$FoxyData_decrypted = $rc4crypt->decrypt($api_key,$FoxyData_encrypted);
+		$FoxyData_decrypted = $this->rc4crypt->decrypt($api_key,$FoxyData_encrypted);
 		$xml = new SimpleXMLElement($FoxyData_decrypted);
 		return $xml;
     }
-    /**
-	* get_datafeed function
-	* get the posted data from foxycart datafeed url
-	* URL was set on your foxycart stopre dashboard
-	* create a cache files for encrypted and decrypted data
-	* @return string 'foxy' if success or errors if failed
-    **/
-    public function get_datafeed() 
-    {
-    	$api_key = $this->modx->getOption('moxycart.api_key'); // your foxy cart datafeed key
-    	if(empty($api_key)) {
-    		$this->__log_errors('Foxycart API Key is empty on your System Setting');
-    	}
 
-		// You can change the following data if you want to customize what data gets written.
-		if($data = $this->modx->getOption('FoxyData', $_POST)) {
-			$xml = $this->decrypt($data,$api_key);
-
-			// processed the xml
-			$this->parseXML($xml);
-
-			return 'foxy';
-
-		} else {
-			$this->__log_errors('Failed to Proccessed Transaction Data Feed.Please check Error Logs on Foxycart Admin');
-			return 'error';
-		}
-    }
 
     /**
     * parse_xml function
@@ -147,26 +69,7 @@ class FC_Datafeed {
 				$output .= '--------------------------------------------' . "\n";
 				*/
 			}
-
-			// create cache files
-			$this->cached_files();
 		}
     }
-
-
-    public function cached_files($xml,$transaction_id) {
-    	// store files on local dir
-			$dom = new DOMDocument('1.0');
-			$dom->preserveWhiteSpace = false;
-			$dom->formatOutput = true;
-			$dom->loadXML($xml->asXML());
-			
-		    $encrypted_cache_key = 'encrypted_txn_'.$transaction_id;
-		    $this->modx->cacheManager->set($encrypted_cache_key, $FoxyData_encrypted, $$this->lifetime, $$this->cache_opts);
-
-		    $decrypted_cache_key = 'decrypted_txn_'.$transaction_id;
-		    $this->modx->cacheManager->set($decrypted_cache_key, $dom->saveXML(), $$this->lifetime, $this->$cache_opts);
-    }
-
 
 }
