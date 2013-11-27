7e1e48764b66e40366aba5edd528b9f064bf1daf
Everett Griffiths <everett@fireproofsocks.com>
2013-11-27 00:15:37 -0500

    user create... for memberships and regular customer tracking.

--- core/components/moxycart/elements/snippets/usercreate.snippet.php
+++ core/components/moxycart/elements/snippets/usercreate.snippet.php
@@ -12,11 +12,14 @@
  * As much as possible, customer data is stored in the default MODX columns, but 
  * some of this needs to be stored in a user's extended fields.
  *
- * Related system settings:
+ * Control parameters
  *
  *  moxycart.user.usergroup id of usergroup where new users should be added.
  *  moxycart.user.role id of role for new users.
- *  moxycart.user.blocked whether or not new users should be blocked (1) or active (0) 
+ *  
+    user.active whether or not new users should be activated immediately
+    user.primary_group
+    
  *  moxycart.user.update_profile whether or not to update user data. 
  *
  * @params everything from a <transaction> node in the Foxycart XML
@@ -51,6 +54,12 @@ $modx->addPackage('moxycart',$core_path.'components/moxycart/model/','moxy_');
       <customer_phone><![CDATA[]]></customer_phone>
       <customer_email><![CDATA[test11@fireproofsocks.com]]></customer_email>
       <customer_ip><![CDATA[75.68.99.192]]></customer_ip>
+
+      <customer_password><![CDATA[X6Mc0MxBq5mnDYZGz5A0qUcEQRI555aHVIKYOU5s1NY=]]></customer_password>
+      <customer_password_salt><![CDATA[b778896af4acfba66f20be655c9a0f9e]]></customer_password_salt>
+      <customer_password_hash_type><![CDATA[pbkdf2]]></customer_password_hash_type>
+      <customer_password_hash_config><![CDATA[1000, 32, sha256]]></customer_password_hash_config>
+      
       <shipping_first_name><![CDATA[Testing]]></shipping_first_name>
       <shipping_last_name><![CDATA[Testerson]]></shipping_last_name>
       <shipping_company><![CDATA[]]></shipping_company>
@@ -61,6 +70,7 @@ $modx->addPackage('moxycart',$core_path.'components/moxycart/model/','moxy_');
       <shipping_postal_code><![CDATA[90403]]></shipping_postal_code>
       <shipping_country><![CDATA[US]]></shipping_country>
       <shipping_phone><![CDATA[]]></shipping_phone>
+      
 */
 
 $email = $modx->getOption('customer_email', $scriptProperties);
@@ -71,7 +81,8 @@ $User = $modx->getObjectGraph('modUser', '{"Profile":{}}',$query);
 
 if ($User) {
     if ($modx->getOption('moxycart.user.update_profile')) {
-        $modx->log(xPDO::LOG_LEVEL_DEBUG,'Updating existing user profile for user '.$User->get('id'),$log,'UserCreate Snippet',__FILE__,__LINE__);    
+        $modx->log(xPDO::LOG_LEVEL_DEBUG,'Updating existing user profile for user '.$User->get('id'),$log,'UserCreate Snippet',__FILE__,__LINE__); 
+        $Profile = $modx->getObject('modUserProfile',array('internalKey' => $User->get('id')));
     }
     else {
         $modx->log(xPDO::LOG_LEVEL_DEBUG,'User already exists with email '.$email. ' (User id '.$User->get('id').')',$log,'UserCreate Snippet',__FILE__,__LINE__);
@@ -83,14 +94,66 @@ else {
     $Profile = $modx->newObject('modUserProfile');
 }
 
+// Primary User Fields
+$User->set('username', $modx->getOption('customer_email',$scriptProperties));
+$User->set('active', $modx->getOption('user.active',$scriptProperties));
+$User->set('primary_group', $modx->getOption('user.primary_group',$scriptProperties));
 
-$fields = array('customer_first_name','customer_last_name','customer_company','customer_address1',
-'customer_address2', 'customer_city','customer_state','customer_postal_code','customer_country','customer_phone',
-'customer_email','customer_ip');
+// Make sure we have the correct hash type...
+if ($modx->getOption('customer_password_hash_type',$scriptProperties) == 'pbkdf2') {
+    $User->set('password', $modx->getOption('customer_password',$scriptProperties));
+    $User->set('salt', $modx->getOption('customer_password_salt',$scriptProperties));    
+    // customer_password_hash_config ??
+}
+// Todo: roll with this?  We need mappings between Foxycart hash types and MODX classnames
+else {
+    $modx->log(xPDO::LOG_LEVEL_DEBUG,'Invalid password hash type detected: '
+        .$modx->getOption('customer_password_hash_type',$scriptProperties)
+        . ' Please update your Foxycart advanced settings to use the pbkdf2'
+        . ' hash type for MODX.'
+        ,$log,'UserCreate Snippet',__FILE__,__LINE__);
+}
+
+
+
+
+// Secondary Fields (Profile)
+$fullname = trim($modx->getOption('customer_first_name',$scriptProperties) . ' '
+    . $modx->getOption('customer_last_name',$scriptProperties));
+$Profile->set('fullname', $fullname);
+$Profile->set('email', $modx->getOption('customer_email',$scriptProperties));
+$Profile->set('phone', $modx->getOption('customer_phone',$scriptProperties));
+$address = trim($modx->getOption('customer_address1',$scriptProperties)."\n"
+    .$modx->getOption('customer_address2',$scriptProperties));
+$Profile->set('address', $address);
+$Profile->set('city', $modx->getOption('customer_city',$scriptProperties));
+$Profile->set('state', $modx->getOption('customer_state',$scriptProperties));
+$Profile->set('zip', $modx->getOption('customer_postal_code',$scriptProperties));
+$Profile->set('country', $modx->getOption('customer_country',$scriptProperties));
+
+// Extended fields
+$extended = array();
+
+$extended['shipping_first_name'] = $modx->getOption('shipping_first_name',$scriptProperties);
+$extended['shipping_last_name'] = $modx->getOption('shipping_last_name',$scriptProperties);
+$extended['shipping_company'] = $modx->getOption('shipping_company',$scriptProperties);
+$extended['shipping_address1'] = $modx->getOption('shipping_address1',$scriptProperties);
+$extended['shipping_address2'] = $modx->getOption('shipping_address2',$scriptProperties);
+$extended['shipping_city'] = $modx->getOption('shipping_city',$scriptProperties);
+$extended['shipping_state'] = $modx->getOption('shipping_state',$scriptProperties);
+$extended['shipping_postal_code'] = $modx->getOption('shipping_postal_code',$scriptProperties);
+$extended['shipping_country'] = $modx->getOption('shipping_country',$scriptProperties);
+$extended['shipping_phone'] = $modx->getOption('shipping_phone',$scriptProperties);
+
+$Profile->set('extended',$extended);
+
+
+/*
 $data = array();
 foreach ($fields as $f) {
     $data[$f] = $modx->getOption($f, $scriptProperties);
 }
+*/
 
 //$User->set('fullname', "{$data['']");
 
