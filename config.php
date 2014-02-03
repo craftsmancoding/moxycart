d00d955fe66719bc8d75a65564d6da533fab8f25
dan.edan <INTEL@INTEL-PC.(none)>
2014-02-04 06:04:39 +0800

    modDashboardPlacement Seed File

--- config.php
+++ config.php
@@ -90,6 +90,19 @@ return array(
                 ),
             ),            
         ),  
+        'modDashboardWidgetPlacement'   => array(
+            xPDOTransport::PRESERVE_KEYS => true,
+            xPDOTransport::UPDATE_OBJECT => true, 
+            xPDOTransport::UNIQUE_KEY => array('widget'),
+            xPDOTransport::RELATED_OBJECTS => true,
+            xPDOTransport::RELATED_OBJECT_ATTRIBUTES    => array(
+                'modDashboardWidget'    =>  array (
+                    xPDOTransport::PRESERVE_KEYS => false,
+                    xPDOTransport::UPDATE_OBJECT => true, 
+                    xPDOTransport::UNIQUE_KEY => array('name'),
+                )
+            )
+        ),   
     ),
 );
 /*EOF*/
---
+++ core/components/moxycart/model/seeds/base/modDashboardWidgetPlacement.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+  array(
+    'dashboard' => 1,
+    'rank' => 1,
+    'Widget' => array(
+        'name' => 'Product Quantity Alert',
+        'description' => 'List all Product which set to track their inventory.',
+        'type' => 'snippet',
+        'content' => 'ProductQtyAlert',
+        'size'  => 'half',
+        'namespace' => 'moxycart',
+        'lexicon' =>  'core:dashboards'
+     )
+  )
+);
+/*EOF*/
