5c2b0c53d9b4f1baef89f926eff71361822d99ff
Everett Griffiths <everett@fireproofsocks.com>
2014-05-26 22:28:55 -0700

    getTaxonomies snippet adjusted

--- elements/snippets/getTaxonomies.php
+++ elements/snippets/getTaxonomies.php
@@ -12,7 +12,10 @@
  * -----------------------------
  * @param string $outerTpl Format the Outer Wrapper of List (Optional)
  * @param string $innerTpl Format the Inner Item of List
- *
+ * @param string $sort column to sort by (default: pagetitle)
+ * @param string $dir ASC (default) or DESC
+ * @param integer $limit the max number of records
+ * @param integer $start the offset
  * Variables
  * ---------
  * @var $modx modX
@@ -30,20 +33,24 @@ require_once $core_path .'vendor/autoload.php';
 $Snippet = new \Moxycart\Snippet($modx);
 $Snippet->log('getTaxonomies',$scriptProperties);
 
+$innerTpl = $modx->getOption('innerTpl', $scriptProperties, '<li>[[+pagetitle]]</li>');
+$outerTpl = $modx->getOption('outerTpl', $scriptProperties, '<ul>[[+content]]</ul>');
+$limit = (int) $modx->getOption('limit', $scriptProperties); 
+$start = (int) $modx->getOption('start', $scriptProperties); 
+$sort = $modx->getOption('sort', $scriptProperties,'pagetitle');
+$dir = $modx->getOption('sort', $scriptProperties,'ASC'); 
+
 $criteria = $modx->newQuery('Taxonomy');
 $criteria->where(array('class_key'=>'Taxonomy'));
-$pages = $modx->getCollection('Taxonomy',$criteria);
 
-$results = array();
-foreach ($pages as $p) {
-    $results[] = array(
-        'id' => $p->get('id'),
-        'pagetitle' => $p->get('pagetitle')
-    );
+if ($limit) {
+    $criteria->limit($limit, $start); 
 }
+$criteria->sortby($sort,$dir);
+$results = $modx->getCollection('Taxonomy',$criteria);
 
-if (!empty($results)) {
+if ($results) {
     return $Snippet->format($results,$innerTpl,$outerTpl);    
 }
 
-$modx->log(\modX::LOG_LEVEL_DEBUG, "No results found",'','getTaxonomies',__LINE__);
+$modx->log(\modX::LOG_LEVEL_DEBUG, "No results found",'','Snippt getTaxonomies');
