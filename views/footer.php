02300bce7ace5d9199f21b0caa76d1cbf1142c3f
Daniel Edano <daniel.edano@gmail.com>
2014-08-20 06:38:27 +0800

    Fix Scrollign issue

--- views/footer.php
+++ views/footer.php
@@ -1,15 +1,20 @@
 
 </div><!-- /moxycart_canvas -->
 
-<div id="moxycart_footer">
+				<div id="moxycart_footer">
 
-	<ul>
-		<li class="moxycart_nav_item"><a class="moxycart_donation_link" href="#">Make a Donation</a></li>
-		<li class="moxycart_nav_item"><a class="moxycart_bug_link" href="https://github.com/craftsmancoding/moxycart/issues/new">Report a Bug</a></li>
-		<li class="moxycart_nav_item"><a class="moxycart_wiki_link" href="https://github.com/craftsmancoding/moxycart/wiki">Wiki</a></li>
-		<li class="moxycart_nav_item"><a class="moxycart_support_link" href="http://craftsmancoding.com/contact">Get Paid Support</a></li>
-		
-	</ul>
-   
-    <div id="moxycart_copyright">&copy; 2014 and beyond by <a href="http://craftsmancoding.com/">Craftsman Coding</a></div>
+					<ul>
+						<li class="moxycart_nav_item"><a class="moxycart_donation_link" href="#">Make a Donation</a></li>
+						<li class="moxycart_nav_item"><a class="moxycart_bug_link" href="https://github.com/craftsmancoding/moxycart/issues/new">Report a Bug</a></li>
+						<li class="moxycart_nav_item"><a class="moxycart_wiki_link" href="https://github.com/craftsmancoding/moxycart/wiki">Wiki</a></li>
+						<li class="moxycart_nav_item"><a class="moxycart_support_link" href="http://craftsmancoding.com/contact">Get Paid Support</a></li>
+						
+					</ul>
+				   
+				    <div id="moxycart_copyright">&copy; 2014 and beyond by <a href="http://craftsmancoding.com/">Craftsman Coding</a></div>
+				</div>
+
+			</div>
+		</div>
+	</div>
 </div>
--- views/header.php
+++ views/header.php
@@ -7,46 +7,53 @@ There are 2 types of links:
 Valid pagename can be any function in the PageController (minus the get/post prefix)
 */
 ?>
-<div id="moxycart_header" class="clearfix">
-    <ul id="moxycart_nav">
-        <li class="moxycart_nav_item">
-            <strong>Manage:</strong>
-        </li>
-        <!--li class="moxycart_nav_item moxycart_nav_home"-->
-            <!--span class="linklike" onclick="javascript:paint('index');">Home</span-->
-            <!--a href="<?php print self::page('index'); ?>">Home</a-->
-        <!--/li-->
-        <li class="moxycart_nav_item">
-            <!--span class="linklike" onclick="javascript:paint('products');">Products</span-->
-            <a class="<?php print ($_GET['method'] == 'products') ? 'current' : '' ; ?>" href="<?php print self::page('products'); ?>">Products</a>
-        
-        </li>
-        <li class="moxycart_nav_item">
-            <!--span class="linklike" onclick="javascript:paint('fields');">Custom Fields</span-->
-            <a class="<?php print ($_GET['method'] == 'fields') ? 'current' : '' ; ?>" href="<?php print self::page('fields'); ?>">Custom Fields</a>
-        </li>
-        <li class="moxycart_nav_item">
-            <!--span class="linklike" onclick="javascript:paint('options');">Options</span-->
-            <a class="<?php print ($_GET['method'] == 'options') ? 'current' : '' ; ?>" href="<?php print self::page('options'); ?>">Options</a>
-        </li>
-        <li class="moxycart_nav_item">
-            <!--span class="linklike" onclick="javascript:paint('reviews');">Reviews</span-->
-            <a class="<?php print ($_GET['method'] == 'reviews') ? 'current' : '' ; ?>" href="<?php print self::page('reviews'); ?>">Reviews</a>
-        </li>
-        <li class="moxycart_nav_item">
-            <!--span class="linklike" onclick="javascript:paint('orders');">Reports</span-->
-            <a class="<?php print ($_GET['method'] == 'orders') ? 'current' : '' ; ?>" href="<?php print self::page('orders'); ?>">Orders</a>
-        </li>
-        <li class="moxycart_nav_item">
-            <!--span class="linklike" onclick="javascript:paint('settings');">Settings</span-->
-            <a class="<?php print ($_GET['method'] == 'settings') ? 'current' : '' ; ?>" href="<?php print self::page('settings'); ?>">Settings</a>
-        </li>
-    </ul>
-</div>
 
-<div id="moxycart_msg"></div>
+<div class="x-panel-bwrap">
+    <div class="x-panel-body x-panel-body-noheader x-panel-body-noborder" >
+        <div class=" x-panel container x-panel-noborder">
+            <div class="x-panel-bwrap">
 
-<div id="moxycart_canvas">
+                
+                    <div id="moxycart_header" class="clearfix">
+                        <ul id="moxycart_nav">
+                            <li class="moxycart_nav_item">
+                                <strong>Manage:</strong>
+                            </li>
+                            <!--li class="moxycart_nav_item moxycart_nav_home"-->
+                                <!--span class="linklike" onclick="javascript:paint('index');">Home</span-->
+                                <!--a href="<?php print self::page('index'); ?>">Home</a-->
+                            <!--/li-->
+                            <li class="moxycart_nav_item">
+                                <!--span class="linklike" onclick="javascript:paint('products');">Products</span-->
+                                <a class="<?php print ($_GET['method'] == 'products') ? 'current' : '' ; ?>" href="<?php print self::page('products'); ?>">Products</a>
+                            
+                            </li>
+                            <li class="moxycart_nav_item">
+                                <!--span class="linklike" onclick="javascript:paint('fields');">Custom Fields</span-->
+                                <a class="<?php print ($_GET['method'] == 'fields') ? 'current' : '' ; ?>" href="<?php print self::page('fields'); ?>">Custom Fields</a>
+                            </li>
+                            <li class="moxycart_nav_item">
+                                <!--span class="linklike" onclick="javascript:paint('options');">Options</span-->
+                                <a class="<?php print ($_GET['method'] == 'options') ? 'current' : '' ; ?>" href="<?php print self::page('options'); ?>">Options</a>
+                            </li>
+                            <li class="moxycart_nav_item">
+                                <!--span class="linklike" onclick="javascript:paint('reviews');">Reviews</span-->
+                                <a class="<?php print ($_GET['method'] == 'reviews') ? 'current' : '' ; ?>" href="<?php print self::page('reviews'); ?>">Reviews</a>
+                            </li>
+                            <li class="moxycart_nav_item">
+                                <!--span class="linklike" onclick="javascript:paint('orders');">Reports</span-->
+                                <a class="<?php print ($_GET['method'] == 'orders') ? 'current' : '' ; ?>" href="<?php print self::page('orders'); ?>">Orders</a>
+                            </li>
+                            <li class="moxycart_nav_item">
+                                <!--span class="linklike" onclick="javascript:paint('settings');">Settings</span-->
+                                <a class="<?php print ($_GET['method'] == 'settings') ? 'current' : '' ; ?>" href="<?php print self::page('settings'); ?>">Settings</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div id="moxycart_msg"></div>
+
+                    <div id="moxycart_canvas">
 
 
     
